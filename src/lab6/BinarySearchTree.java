f8d8a512453425f0c2abcdb159dce8440f95067e
griffgruffjava <C_Train@10.0.1.8>
2015-12-06 22:08:21 +0000

    finished lab

--- src/lab6/BinarySearchTree.java
+++ src/lab6/BinarySearchTree.java
@@ -4,64 +4,51 @@
  */
 package lab6;
 
-public class BinarySearchTree<E extends Comparable<E>>
-{
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class BinarySearchTree<E extends Comparable<E>> {
 
     private Node root;
 
-    private class Node
-    {
+    private class Node {
 
         public E data;
         public Node left;
         public Node right;
     }
 
-    public BinarySearchTree()
-    {
+    public BinarySearchTree() {
         root = null;
     }
 
-    public void insert(E e)
-    {
+    public void insert(E e) {
         Node newNode = new Node();
         newNode.data = e;
         newNode.left = null;
         newNode.right = null;
-        if (root == null)
-        {
+        if (root == null) {
             root = newNode;
-        } else
-        {
+        } else {
             insertSub(root, newNode);
         }
     }
 
-    private void insertSub(Node node, Node newNode)
-    {
-        if (newNode.data.compareTo(node.data) < 0)
-        {
-            if (node.left == null)
-            {
+    private void insertSub(Node node, Node newNode) {
+        if (newNode.data.compareTo(node.data) < 0) {
+            if (node.left == null) {
                 node.left = newNode;
-            } else
-            {
+            } else {
                 insertSub(node.left, newNode);
             }
-        } else
-        {
-            if (node.right == null)
-            {
-                node.right = newNode;
-            } else
-            {
-                insertSub(node.right, newNode);
-            }
+        } else if (node.right == null) {
+            node.right = newNode;
+        } else {
+            insertSub(node.right, newNode);
         }
     }
 
-    private void insertIter(E e)
-    {
+    private void insertIter(E e) {
         Node newNode = new Node();
         newNode.data = e;
         newNode.left = null;
@@ -69,82 +56,78 @@ public class BinarySearchTree<E extends Comparable<E>>
 
         Node nextNode = new Node();
         nextNode = root;
-        
-        
-        
-
     }
 
-    public void print()
-    {
-        if (root != null)
-        {
+    public void preOrderPrintIter() {
+        Deque<Node> stack = new LinkedList<Node>();
+        
+            stack.push(root);
+            
+            while(stack.size()>0)
+            {
+                Node currentNode = stack.pop();
+                System.out.println(currentNode.data);
+                if(currentNode.left!=null)
+                    stack.push(currentNode.left);
+                if(currentNode.right!=null)
+                    stack.push(currentNode.right);
+            }
+        
+    }
+
+    public void print() {
+        if (root != null) {
             printSub(root);
         }
     }
 
-    private void printSub(Node node)
-    {
-        if (node != null)
-        {
+    private void printSub(Node node) {
+        if (node != null) {
             printSub(node.left);
             System.out.println(node.data);
             printSub(node.right);
         }
     }
 
-    public void printPre()
-    {
-        if (root != null)
-        {
+    public void printPre() {
+        if (root != null) {
             printSubPre(root);
         }
     }
 
-    private void printSubPre(Node node)
-    {
-        if (node != null)
-        {
+    private void printSubPre(Node node) {
+        if (node != null) {
             System.out.println(node.data);
             printSubPre(node.left);
             printSubPre(node.right);
         }
     }
 
-    public void printPost()
-    {
-        if (root != null)
-        {
+    public void printPost() {
+        if (root != null) {
             printSubPost(root);
         }
     }
 
-    private void printSubPost(Node node)
-    {
-        if (node != null)
-        {
+    private void printSubPost(Node node) {
+        if (node != null) {
             printSubPost(node.left);
             printSubPost(node.right);
             System.out.println(node.data);
         }
     }
 
-    public boolean contains(E e)
-    {
+    public boolean contains(E e) {
         Node current = root;
 
-        while (current != null)
-        {
+        while (current != null) {
             int result = current.data.compareTo(e);
 
-            if (result == 0)
-            {
+            if (result == 0) {
                 return true;
-            } else if (result < 0)
-            {
+            } else if (result < 0) {
                 current = current.right;
-            } else
-            {
+            } else {
                 current = current.left;
             }
 
@@ -153,31 +136,24 @@ public class BinarySearchTree<E extends Comparable<E>>
         return false;
     }
 
-    public boolean containsRecursive(E e)
-    {
+    public boolean containsRecursive(E e) {
 
-        if (root != null)
-        {
+        if (root != null) {
 
             return subContainsRecursive(root, e);
         }
         return false;
     }
 
-    private boolean subContainsRecursive(Node n, E e)
-    {
-        if (n != null)
-        {
+    private boolean subContainsRecursive(Node n, E e) {
+        if (n != null) {
             int result = n.data.compareTo(e);
 
-            if (result == 0)
-            {
+            if (result == 0) {
                 return true;
-            } else if (result < 0)
-            {
+            } else if (result < 0) {
                 return subContainsRecursive(n.right, e);
-            } else
-            {
+            } else {
                 return subContainsRecursive(n.left, e);
             }
         }
--- test/lab6/BinarySearchTreeTest.java
+++ test/lab6/BinarySearchTreeTest.java
@@ -16,47 +16,158 @@ import static org.junit.Assert.*;
  *
  * @author Finbar
  */
-public class BinarySearchTreeTest
-{
-    
-    public BinarySearchTreeTest()
-    {
+public class BinarySearchTreeTest {
+
+    public BinarySearchTreeTest() {
     }
-    
+
     @BeforeClass
-    public static void setUpClass()
-    {
+    public static void setUpClass() {
     }
-    
+
     @AfterClass
-    public static void tearDownClass()
-    {
+    public static void tearDownClass() {
     }
-    
+
     @Before
-    public void setUp()
-    {
+    public void setUp() {
     }
-    
+
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
     }
 
     /**
      * Test of insert method, of class BinarySearchTree.
      */
+//    @Test
+//    public void testInsert() {
+//        System.out.println("insert");
+//        String a = "hello";
+//        String b = "help";
+//        String c = "hippo";
+//        String d = "Hand";
+//        String e = "hand";
+//        String f = "helper";
+//        String g = "harper";
+//        BinarySearchTree<String> instance = new BinarySearchTree();
+//        instance.insert(a);
+//        instance.insert(b);
+//        instance.insert(c);
+//        instance.insert(d);
+//        instance.insert(e);
+//        instance.insert(f);
+//        instance.insert(g);
+//
+//    }
+//
+//    /**
+//     * Test of print method, of class BinarySearchTree.
+//     */
+//    @Test
+//    public void testPrint() {
+//        System.out.println("print");
+//        BinarySearchTree instance = new BinarySearchTree();
+//        instance.print();
+//        // TODO review the generated test code and remove the default call to fail.
+////        fail("The test case is a prototype.");
+//    }
+
+    /**
+     * Test of contains method, of class BinarySearchTree.
+     */
+//    @Test
+//    public void testContains() {
+//        System.out.println("contains interative");
+//        String a = "hello";
+//        String b = "help";
+//        String c = "hippo";
+//        String d = "hi";
+//        String e = "me";
+//        String f = "moe";
+//        String g = "bath";
+//        String h = "french";
+//        String i = "late";
+//        String j = "june";
+//        String k = "dog";
+//        String l = "jason";
+//        BinarySearchTree<String> instance = new BinarySearchTree();
+//        instance.insert(a);
+//        instance.insert(b);
+//        instance.insert(c);
+//        instance.insert(d);
+//        instance.insert(e);
+//        instance.insert(f);
+//        instance.insert(g);
+//        instance.insert(h);
+//        instance.insert(i);
+//        instance.insert(j);
+//        instance.insert(k);
+//        instance.insert(l);
+//
+//        for (int x = 0; x < 300000; x++) {
+//            boolean expResult = true;
+//            boolean result = instance.contains("jason");
+//            assertEquals(expResult, result);
+//        }
+//
+//    }
+//
+//    /**
+//     * Test of containsRecursive method, of class BinarySearchTree.
+//     */
+//    @Test
+//    public void testContainsRecursive() { //recursive is the faster of the two contains methods
+//        System.out.println("containsRecursive");
+//        String a = "hello";
+//        String b = "help";
+//        String c = "hippo";
+//        String d = "hi";
+//        String e = "me";
+//        String f = "moe";
+//        String g = "bath";
+//        String h = "french";
+//        String i = "late";
+//        String j = "june";
+//        String k = "dog";
+//        String l = "jason";
+//        BinarySearchTree<String> instance = new BinarySearchTree();
+//        instance.insert(a);
+//        instance.insert(b);
+//        instance.insert(c);
+//        instance.insert(d);
+//        instance.insert(e);
+//        instance.insert(f);
+//        instance.insert(g);
+//        instance.insert(h);
+//        instance.insert(i);
+//        instance.insert(j);
+//        instance.insert(k);
+//        instance.insert(l);
+//
+//        for (int x = 0; x < 300000; x++) {
+//            boolean expResult = true;
+//            boolean result = instance.containsRecursive("jason");
+//            assertEquals(expResult, result);
+//        }
+//
+//    }
     @Test
-    public void testInsert()
+    public void testPreOrderMethods()
     {
-        System.out.println("insert");
-        String  a = "hello";
-        String  b = "help";
-        String  c = "hippo";
-        String  d = "Hand";
-        String  e = "hand";
-        String  f = "helper";
-        String  g = "harper";
+        System.out.println("testPreOrderMethods");
+        String a = "hello";
+        String b = "help";
+        String c = "hippo";
+        String d = "hi";
+        String e = "me";
+        String f = "moe";
+        String g = "bath";
+        String h = "french";
+        String i = "late";
+        String j = "june";
+        String k = "dog";
+        String l = "jason";
         BinarySearchTree<String> instance = new BinarySearchTree();
         instance.insert(a);
         instance.insert(b);
@@ -65,62 +176,14 @@ public class BinarySearchTreeTest
         instance.insert(e);
         instance.insert(f);
         instance.insert(g);
+        instance.insert(h);
+        instance.insert(i);
+        instance.insert(j);
+        instance.insert(k);
+        instance.insert(l);
         
-        
+        instance.preOrderPrintIter();
+        System.out.println("\n\n");
+        instance.printPre();
     }
-
-    /**
-     * Test of print method, of class BinarySearchTree.
-     */
-    @Test
-    public void testPrint()
-    {
-        System.out.println("print");
-        BinarySearchTree instance = new BinarySearchTree();
-        instance.print();
-        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of contains method, of class BinarySearchTree.
-     */
-    @Test
-    public void testContains()
-    {
-        System.out.println("contains");
-        String  a = "hello";
-        String  b = "help";
-        String  c = "hippo";
-        BinarySearchTree<String> instance = new BinarySearchTree();
-        instance.insert(a);
-        instance.insert(b);
-        instance.insert(c);
-        
-        boolean expResult = true;
-        boolean result = instance.contains("help");
-        assertEquals(expResult, result);
-        
-    }
-
-    /**
-     * Test of containsRecursive method, of class BinarySearchTree.
-     */
-    @Test
-    public void testContainsRecursive()
-    {
-        System.out.println("containsRecursive");
-        String  a = "hello";
-        String  b = "help";
-        String  c = "hippo";
-        BinarySearchTree<String> instance = new BinarySearchTree();
-        instance.insert(a);
-        instance.insert(b);
-        instance.insert(c);
-        
-        boolean expResult = true;
-        boolean result = instance.containsRecursive("help");
-        assertEquals(expResult, result);
-    }
-    
 }
